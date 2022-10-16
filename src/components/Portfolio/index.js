cc46543dc13437537a0a25651528209475b67851
Mayfield Mayfield <mayfieldmel@gmail.com>
2022-10-15 21:37:26 -0400

    projects render & style for hover

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,63 +1,90 @@
-import React from "react";
+import React, {useState} from "react";
 import Pawfect from "../../assets/images/pawfect-homepage.png";
 
-const projects = [
-  {
-    src: Pawfect,
-    title: "Pawfect Friends",
-    tools:
-      "HTML/Handlebars, CSS/Bulma, JavaScript, Node.js, Express.js, MySQL, Sequelize",
-  },
-  {
-    src: Pawfect,
-    title: "Pawfect Friends",
-    tools:
-      "HTML/Handlebars, CSS/Bulma, JavaScript, Node.js, Express.js, MySQL, Sequelize",
-  },
-  {
-    src: Pawfect,
-    title: "Pawfect Friends",
-    tools:
-      "HTML/Handlebars, CSS/Bulma, JavaScript, Node.js, Express.js, MySQL, Sequelize",
-  },
-  {
-    src: Pawfect,
-    title: "Pawfect Friends",
-    tools:
-      "HTML/Handlebars, CSS/Bulma, JavaScript, Node.js, Express.js, MySQL, Sequelize",
-  },
-  {
-    src: Pawfect,
-    title: "Pawfect Friends",
-    tools:
-      "HTML/Handlebars, CSS/Bulma, JavaScript, Node.js, Express.js, MySQL, Sequelize",
-  },
-  {
-    src: Pawfect,
-    title: "Pawfect Friends",
-    tools:
-      "HTML/Handlebars, CSS/Bulma, JavaScript, Node.js, Express.js, MySQL, Sequelize",
-  },
-];
 
 function Portfolio() {
-  // jsx
-  return (
-    <section>
-      <h2>Portfolio</h2>
-      <div className="portfolio-section">
-        {projects.map((project) => (
-          <div key={project.title}>
-            <img
-              src={`${project.src}`}
-              alt="Pawfect Friends Homepage"
-              className="image"
-            ></img>
+   
+  const projects = [
+    {
+      src: Pawfect,
+      title: "Pawfect Friends",
+      tools:
+        "HTML/Handlebars, CSS/Bulma, JavaScript, Node.js, Express.js, MySQL, Sequelize",
+    },
+    {
+      src: Pawfect,
+      title: "Pawfect Friends",
+      tools:
+        "HTML/Handlebars, CSS/Bulma, JavaScript, Node.js, Express.js, MySQL, Sequelize",
+    },
+    {
+      src: Pawfect,
+      title: "Pawfect Friends",
+      tools:
+        "HTML/Handlebars, CSS/Bulma, JavaScript, Node.js, Express.js, MySQL, Sequelize",
+    },
+    {
+      src: Pawfect,
+      title: "Pawfect Friends",
+      tools:
+        "HTML/Handlebars, CSS/Bulma, JavaScript, Node.js, Express.js, MySQL, Sequelize",
+    },
+    {
+      src: Pawfect,
+      title: "Pawfect Friends",
+      tools:
+        "HTML/Handlebars, CSS/Bulma, JavaScript, Node.js, Express.js, MySQL, Sequelize",
+    },
+    {
+      src: Pawfect,
+      title: "Pawfect Friends",
+      tools:
+        "HTML/Handlebars, CSS/Bulma, JavaScript, Node.js, Express.js, MySQL, Sequelize",
+    },
+  ];
+
+  const [projectCard, setProjectCard] = useState(projects[0])
+  const [projectDetails, setProjectDetails] = useState(false)
+
+//jsx
+return (
+  <section>
+    <h2>Portfolio</h2>
+    <div className="portfolio-section">
+    {projects.map((project) => (
+        <div key={project.title}>
+          <img
+            src={`${project.src}`}
+            alt="Pawfect Friends Homepage"
+            className={`image ${projectCard.title === project.title && projectDetails && 'image-hover'}`}
+            onMouseEnter={() => {setProjectCard(project); setProjectDetails(true)}}
+            onMouseLeave={() => {setProjectCard(project); setProjectDetails(false)}}
+          >
+          </img>
+        </div>
+      ))}
+    </div>
+    
+  <div className="portfolio-section">
+    {projects.map((project) => (
+        <div key={project.title} className="container">
+          <img
+            src={`${project.src}`}
+            alt="Pawfect Friends Homepage"
+            className="img"
+            onMouseEnter={() => {setProjectCard(project); setProjectDetails(true)}}
+            onMouseLeave={() => {setProjectCard(project); setProjectDetails(false)}}
+          >
+          </img>
+          <div className="project-details">
+            <h3>{project.title}</h3>
+            <p className="tools">{project.tools}</p>
           </div>
-        ))}
-      </div>
-    </section>
-  );
+        </div>
+      ))}
+    </div>
+</section>
+)
 }
 
 export default Portfolio;
